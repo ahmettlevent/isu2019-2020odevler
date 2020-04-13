5853f8f62f4ac79df0cbbc2e9b20a423123078b2
ahmettlevent <55196024+ahmettlevent@users.noreply.github.com>
2020-04-14 02:39:30 +0300

    edit

--- 2.donemodevleri/MP's/MP2/YAZ104_MP2/Ahmet-Utku-Levent_190701134_mp2.py
+++ 2.donemodevleri/MP's/MP2/YAZ104_MP2/Ahmet-Utku-Levent_190701134_mp2.py
@@ -5,6 +5,8 @@ from tkinter import ttk, filedialog, messagebox
 
 import xlrd
 
+import recommendations
+
 
 class IstinyeKafeterya(Frame):
     def __init__(self, parent):
@@ -13,12 +15,12 @@ class IstinyeKafeterya(Frame):
 
         self.yemekPuani = DoubleVar()
         self.YemekListesi = list()
-        self.KullaniciPuanlari_Dic = dict()
+        self.BenzerlikOlcudu_Var = IntVar()
 
         self.ToplamOneriAdedi = IntVar()
-
-        self.OneriModeli_var = BooleanVar()
-        self.BenzerlikOlcudu_Var = BooleanVar()
+        self.OneriModeli_var = IntVar()
+        self.BenzerlikOlcudler = [recommendations.sim_distance, recommendations.sim_pearson,
+                                  recommendations.sim_jaccard]
 
         self.databaseAC()
         self.inituI()
@@ -72,24 +74,32 @@ class IstinyeKafeterya(Frame):
                                                                                          height=20)
 
         #                    Öneri Modeli
+        Button(self.parent, text="Öneri Al", command=self.oneriAl, bg="dark green", fg="white").place(x=210, y=318,
+                                                                                                      width=75,
+                                                                                                      height=30)
         Label(self.parent, bg="peach puff", fg="gray5", text="Öneri Modeli", font="10").place(x=55, y=182)
 
         Radiobutton(self.parent, bg="misty rose", activebackground="misty rose", text="Kullanıcı Bazlı",
-                    variable=self.OneriModeli_var, value=1).place(x=55, y=210, width=95)
+                    variable=self.OneriModeli_var, value=0).place(x=55, y=210, width=95)
 
         Radiobutton(self.parent, bg="misty rose", activebackground="misty rose", text="Ürün Bazlı",
-                    variable=self.OneriModeli_var, value=2).place(x=47, y=230, width=95)
+                    variable=self.OneriModeli_var, value=1).place(x=47, y=230, width=95)
 
         #                   Benzerlik Olcudu
+
+        Button(self.parent, text="Benzer Müşterileri Listele", command=self.benzerMusterileriListele, bg="dark green",
+               fg="white").place(
+            x=478, y=318, width=145, height=30)
+
         Label(self.parent, bg="peach puff", fg="gray5", text="Benzerlik Ölçütü", font="10").place(x=275, y=182)
 
         Radiobutton(self.parent, bg="misty rose", activebackground="misty rose", text="Öklid",
-                    variable=self.BenzerlikOlcudu_Var, value=1).place(x=260, y=210, width=95)
+                    variable=self.BenzerlikOlcudu_Var, value=0).place(x=260, y=210, width=95)
 
         Radiobutton(self.parent, bg="misty rose", activebackground="misty rose", text="Pearson",
-                    variable=self.BenzerlikOlcudu_Var, value=2).place(x=267, y=230, width=95)
-        Radiobutton(self.parent, bg="misty rose", activebackground="misty rose", text="Jaqqard",
-                    variable=self.BenzerlikOlcudu_Var, value=3).place(x=266, y=250, width=95)
+                    variable=self.BenzerlikOlcudu_Var, value=1).place(x=267, y=230, width=95)
+        Radiobutton(self.parent, bg="misty rose", activebackground="misty rose", text="Jaccard",
+                    variable=self.BenzerlikOlcudu_Var, value=2).place(x=266, y=250, width=95)
         # ----
 
         # Çizgi ve Toplam Öneri Adedi
@@ -97,7 +107,7 @@ class IstinyeKafeterya(Frame):
         Label(self.parent, bg="peach puff", text="Toplam Öneri Adedi", font="20").place(x=55, y=285,
                                                                                         width=150,
                                                                                         height=25)
-        Entry(self.parent, textvariable=self.ToplamOneriAdedi).place(x=215, y=288, width=50,height=20)
+        Entry(self.parent, textvariable=self.ToplamOneriAdedi).place(x=215, y=288, width=50, height=20)
         Label(self.parent, bg="olive drab").place(x=0, y=310, width=800, height=3)
         #
 
@@ -113,6 +123,7 @@ class IstinyeKafeterya(Frame):
         self.scrollbarekle(self.BenzerMusteriler_Listbox)
 
     def databaseAC(self):
+        # Dosyalarda Karışıklık olmaması için database bir alt klasöre açılmıştır .
         import os
         dirname = "190701134_UserDATABASE"
         try:
@@ -122,56 +133,59 @@ class IstinyeKafeterya(Frame):
             self.user_data = dbm.open("{}/KullaniciDegerlendirmeleri".format(dirname), "w")
 
     def excel_sec(self):
+        # Excel Seçimi ve Kontrolü
         try:
             self.excelFile_path = filedialog.askopenfilename(initialdir="/", title="Dosya Sec",
                                                              filetypes=(("Excel file", "*.xlsx"),))
             self.yemekListesiCikar()
             self.alertlabel.destroy()
         except IOError:
-            messagebox.showwarning("Lütfen Geçerli Bir Dosya Seçin ! ")
+            messagebox.showwarning("Uyarı ", "Lütfen Geçerli Bir Dosya Seçin ! ")
 
     def yemekListesiCikar(self):
+        # Yemek Listesi Seçim Ekranı
         file = xlrd.open_workbook(self.excelFile_path)
         sayfa = file.sheet_by_name(file.sheet_names()[0])
+
+        # Yemekler Sonra Kullanılmak için YemekListesi isimli bir değişkene(küme) kaydedilmiştir.
         for i in range(1, sayfa.nrows):
             if sayfa.row(i)[1].value not in self.YemekListesi:
                 self.YemekListesi.append(sayfa.row(i)[1].value)
+        # Seçim ile Birlikte Arayüzde bazı değişiklikler
         self.combobox.set("Yemekler")
         self.combobox.config(values=self.YemekListesi)
         self.combobox.update()
         Label(self.parent, bg="misty rose", text="HAZIR !").place(x=4, y=110)
 
-    def musteriDegerlendirmeleriniCikar(self):
-        file = xlrd.open_workbook(self.excelFile_path)
-        sayfa = file.sheet_by_name(file.sheet_names()[0])
-
-        yemek_puanlari = {}  # Her bir yemek icin kullanicilarin verdigi puanlar
-        for a in range(1, sayfa.nrows):
-            for i in range(1, sayfa.nrows):
-                yemek_puanlari[sayfa.row(i)[0].value] = sayfa.row(i)[2].value  # yemek icin verilen puanlar sozlukte
-            self.KullaniciPuanlari_Dic[sayfa.row(a)[1].value] = yemek_puanlari  # genel sozluge eklendi
-            yemek_puanlari = {}  # onceki yemegin puanlari silinmistir
-
     def puanlanmisYemekListesiDuzenle(self):
+        # Bu Fonksiyon Yemek listesi üzerinde gerçekleşen her bir işlemden sonra yemek listesinin sıfırdan düzenlenmesi
+        # için çağırılır.
         self.puanlanmisyemekler_listbox.delete(0, END)
         for i in self.user_data:
-            food_text = pickle.loads(i) + " - Puan : " + str(pickle.loads(self.user_data[i]))
+            food_text = pickle.loads(i) + "- Puan : " + str(pickle.loads(self.user_data[i]))
             self.puanlanmisyemekler_listbox.insert(END, food_text)
 
     def puan_kaldir(self):
         try:
-            yemek = self.puanlanmisyemekler_listbox.selection_get()
-            del self.user_data[pickle.dumps(yemek[:-12])]
+            yemek = self.puanlanmisyemekler_listbox.selection_get()  # seçilen yemek
+
+            # seçilen yemekten puan bilgisi atılıp database de aranır ve silinir
+            del self.user_data[pickle.dumps(yemek.split("-")[0])]
+            # o an seçili olan yemek listbox tan silinir
             self.puanlanmisyemekler_listbox.delete(self.puanlanmisyemekler_listbox.curselection())
-        except:
+
+        except TclError:
             messagebox.showinfo("Uyarı", "Lütfen Silmek Istediginiz Yemegi Secin")
 
     def puan_ekle(self):
+
         try:
-            if len(self.combobox.get()) > 0 and self.combobox.get() in self.YemekListesi:
-                if self.yemekPuani.get() > 10 or self.yemekPuani.get() < 0:
+            if self.combobox.get() in self.YemekListesi:  # Yemek seçim kontrolü
+                if self.yemekPuani.get() > 10 or self.yemekPuani.get() < 0:  # Kullanıcı puan kontrolü
                     messagebox.showwarning("Uyarı !", "Lütfen 0 ile 10 arasinda bir puan girin.")
                 else:
+                    # Combobox'tan Yemek , Entry'den Puan bilgisi alınıp Database'e eklenmiş ve
+                    # Yemek Listbox'ı düzenlenmek üzere ilgili fonksiyon çağırılmıştır.
                     self.user_data[pickle.dumps(self.combobox.get())] = pickle.dumps(self.yemekPuani.get())
                     self.puanlanmisYemekListesiDuzenle()
             else:
@@ -179,7 +193,114 @@ class IstinyeKafeterya(Frame):
         except TclError:
             messagebox.showwarning("Uyarı !", "Puan Alanı BOŞ Bırakılamaz !!")
 
+    def oneriAl(self):
+        try:
+            # Toplam öneri Adedinin 0 dan büyük olması beklenmektedir.
+            if self.ToplamOneriAdedi.get() == 0:
+                raise TclError
+
+            self.OneriAl_Listbox.delete(0, END)  # Öneri listbox ı temizlendi
+
+            # OneriModeli_var eğer 0 ise Kullanıcı Bazlı öneri , 1 ise Ürün bazlı öneri yapılacaktır.
+            # Öncelikle öneri fonksiyonlarından geri dönen değer bir sözlük yapısına dönüştürülüp , sadece Yemek
+            # isimler alınmıştır daha sonra bu değerler kullanıcının isteğine göre iteration yöntemi ile alınmıştır
+
+            if self.OneriModeli_var.get() == 0:
+                # init fonksiyonunda benzerlik ölçütleri isimli bir liste değişkeninde ilgili benzerlik ölçütleri
+                # fonksiyonları bulunmaktadır ,bu kısımda kullanıcının seçtiği ölçüte göre listenin o indexindeki
+                # fonksiyon , similarity parmetresinin yerini almıştır.
+
+                iterator = dict(recommendations.getRecommendations(self.AllCritics(), "Kullanici",
+                                                                   similarity=self.BenzerlikOlcudler[
+                                                                       self.BenzerlikOlcudu_Var.get()])).values().__iter__()
+                for i in range(self.ToplamOneriAdedi.get()):
+                    self.OneriAl_Listbox.insert(END, iterator.__next__())
+
+            elif self.OneriModeli_var.get() == 1:
+
+                itemsimilarity = recommendations.calculateSimilarItems(self.AllCritics())
+                iterator = dict(recommendations.getRecommendedItems(self.AllCritics(), itemsimilarity,
+                                                                         "Kullanici")).values().__iter__()
+
+                for i in range(self.ToplamOneriAdedi.get()):
+                    self.OneriAl_Listbox.insert(END, iterator.__next__())
+
+        except TclError:
+            messagebox.showerror("Uyarı !", "Lütfen 0'dan Büyük Bir Öneri Adedi Giriniz !")
+        except StopIteration:
+            pass
+        except FileNotFoundError:
+            messagebox.showwarning("Uyarı ", "Lütfen Geçerli Bir Dosya Seçin ! ")
+        except TypeError:
+            pass
+
+    def benzerMusterileriListele(self):
+
+        try:
+            # Toplam öneri Adedinin 0 dan büyük olması beklenmektedir.
+            if self.ToplamOneriAdedi.get() == 0:
+                raise TclError
+
+            c = 0  # Kullanıcıya Bilgi Amaçlı Counter
+
+            # Öncelikle öneri fonksiyonlarından geri dönen değer bir sözlük yapısına dönüştürülüp , sadece Müşteri
+            # isimler alınmıştır daha sonra bu değerler kullanıcının adet isteğine göre iteration yöntemi ile alınmıştır
+
+            # init fonksiyonunda benzerlik ölçütleri isimli bir liste değişkeninde ilgili benzerlik ölçütleri
+            # fonksiyonları bulunmaktadır ,bu kısımda kullanıcının seçtiği ölçüte göre listenin o indexindeki
+            # fonksiyon , similarity parmetresinin yerini almıştır.
+
+            self.BenzerMusteriler_Listbox.delete(0, END)
+            benzerlikOlcudu = self.BenzerlikOlcudler[self.BenzerlikOlcudu_Var.get()]
+
+            # Müşteriler ve Kullanıcı ile arasındaki mesafeler listbox'a eklenmiştir.
+            iterator = dict(recommendations.topMatches(self.AllCritics(), "Kullanici",
+                                                       similarity=benzerlikOlcudu)).keys().__iter__()
+
+            iterator1 = dict(recommendations.topMatches(self.AllCritics(), "Kullanici",
+                                                       similarity=benzerlikOlcudu)).values().__iter__()
+
+            for i in range(self.ToplamOneriAdedi.get()):
+                c += 1
+                self.BenzerMusteriler_Listbox.insert(END,"{}.Sırada {} - Aradaki Mesafe = {}".format(c,iterator1.__next__()
+                                                                                            ,str(iterator.__next__())[:4]))
+
+        except StopIteration:
+            pass
+        except TclError:
+            messagebox.showerror("Uyarı !", "Lütfen 0'dan Büyük Bir Öneri Adedi Giriniz !")
+        except FileNotFoundError:
+            messagebox.showwarning("Uyarı ", "Lütfen Geçerli Bir Dosya Seçin ! ")
+        except TypeError:
+            pass
+
+    def AllCritics(self):
+        # Excelden alınan veriler ile Critis sözlüğü oluşturulmuş ve Kullanıcının verileri sözlüğün içine aktarılmıştır.
+        try:
+            file = xlrd.open_workbook(self.excelFile_path)
+            sayfa = file.sheet_by_name(file.sheet_names()[0])
+
+            KullaniciPuanlari_Dic = dict()
+            KullaniciPuanlari_Dic["Kullanici"] = {}
+
+            for i in range(1, sayfa.nrows):
+                KullaniciPuanlari_Dic[sayfa.row(i)[0].value] = {}
+            for i in range(1, sayfa.nrows):
+                KullaniciPuanlari_Dic[sayfa.row(i)[0].value][sayfa.row(i)[1].value] = sayfa.row(i)[2].value
+
+            for i in self.user_data:
+                KullaniciPuanlari_Dic["Kullanici"][pickle.loads(i)] = pickle.loads(self.user_data[i])
+
+            return KullaniciPuanlari_Dic
+        except AttributeError:
+            messagebox.showwarning("Uyarı !!", "Lütfen Bir Excel SEÇİNİZ !!")
+        # -----------
+
+    def tersYuzCritics(self):
+        return recommendations.transformPrefs(self.AllCritics())
+
     def scrollbarekle(self, Listbox):
+        # Listbox'a scrollbar ekleme işlemini kolaylaştırmak amaçlı oluşturulmuş bir fonksiyon.
         scrollbar_y = Scrollbar(Listbox, width=12)
         scrollbar_y.pack(side=RIGHT, fill=Y)
         scrollbar_y.config(command=Listbox.yview)
--- 2.donemodevleri/MP's/MP2/YAZ104_MP2/recommendations.py
+++ 2.donemodevleri/MP's/MP2/YAZ104_MP2/recommendations.py
@@ -1,19 +1,24 @@
 # A dictionary of movie critics and their ratings of a small
 # set of movies
-critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5},
-           'Gene Seymour': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
-                            'Just My Luck': 3.0, 'Superman Returns': 3.5, 'The Night Listener': 3.0,
-                            'You, Me and Dupree': 2.5},
+critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+                         'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
+                         'The Night Listener': 3.0},
+           'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+                            'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
+                            'You, Me and Dupree': 3.5},
            'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
                                 'Superman Returns': 3.5, 'The Night Listener': 4.0},
-           'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0},
+           'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+                            'The Night Listener': 4.5, 'Superman Returns': 4.0,
+                            'You, Me and Dupree': 2.5},
            'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
                             'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
                             'You, Me and Dupree': 2.0},
            'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
                              'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
-           'Toby': {'Snakes on a Plane': 4.5, 'Superman Returns': 4.0},
-           'Ali': {'Just My Luck': 1.0, 'You, Me and Dupree': 3.5}}
+           'Toby': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0},
+           'Ali': {'Just My Luck': 1.0, 'You, Me and Dupree': 2.0, 'The Night Listener': 4.5, 'Superman Returns': 1.0,
+                   'Snakes on a Plane': 4.5}}
 
 from math import sqrt
 
@@ -24,7 +29,7 @@ def sim_jaccard(prefs, person1, person2):  # Jaccard Distance (A, B) = |A inters
         if item in prefs[person2]: p1_intersect_p2[item] = 1
 
     # Get the list of all items that we have
-    p1_union_p2 = prefs[person1]
+    p1_union_p2 = dict(prefs[person1])
     for item in prefs[person2]:
         if item not in p1_union_p2: p1_union_p2[item] = 1
 
@@ -103,11 +108,9 @@ def getRecommendations(prefs, person, similarity=sim_pearson):
         # don't compare me to myself
         if other == person: continue
         sim = similarity(prefs, person, other)
-
         # ignore scores of zero or lower
         if sim <= 0: continue
         for item in prefs[other]:
-
             # only score movies I haven't seen yet
             if item not in prefs[person] or prefs[person][item] == 0:
                 # Similarity * Score
