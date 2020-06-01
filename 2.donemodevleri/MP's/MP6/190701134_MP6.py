ab613f32534c033bbfe33fefc12d91899c507150
ahmettlevent <ahmettlevent@users.noreply.github.com>
2020-06-01 13:31:23 +0300

    Mp6-edit

--- 2.donemodevleri/MP's/MP6/190701134_MP6.py
+++ 2.donemodevleri/MP's/MP6/190701134_MP6.py
@@ -11,18 +11,78 @@ class IstinyeDersAnaliz():
                                      "ym": "yazilim-muhendisligi"}
         self.show_data_matrix()
 
-    def show_data_matrix(self,):
+    def show_data_matrix(self):
         ''' Bu fonksiyon benzlerlik ölçütü ve dersleri UI'den alip gerekli fonksiyonların çağırılmasından sorumludur'''
         lessons = ["bm", "mm"]  # Geçici bölüm listesi
+
+        all_lesson_details = self.all_lessons(lessons)
+
+        rownames, colnames, data = self.cook_data(all_lesson_details)
+        self.show_screen(rownames, colnames, data)
+
+    ''' 1.Seviye Fonksiyonlar'''
+
+    def all_lessons(self, lessons):
+        # ready
         all_lessons = {}
         for i in lessons:
             lessonpage = self.get_page(i)
             lessons_dic = self.get_lesson_content(lessonpage)
             for i in lessons_dic:
-                all_lessons[i] = {"name": lessons_dic[i]["name"], "content": lessons_dic[i]["content"]}
-        rownames, colnames, data = self.make_data(all_lessons)
+                all_lessons[i] = {"code": lessons_dic[i]["code"], "name": lessons_dic[i]["name"],
+                                  "content": lessons_dic[i]["content"]}
+        return all_lessons
+
+    def show_screen(self, rownames, colnames, data):
+        print(colnames, end=" ")
+        print("\n")
+        for i in range(len(rownames)):
+            print("")
+            print(rownames[i], end=" ")
+            print(len(data))
+            for a in data[i]:
+                print("" + str(a), end=" ")
+
+    def cook_data(self, lessons_dic):
+        rownames, colnames, data = [], [], []
+        allwords = set()
+        illegal_words = ["", " ", "."]  # Degerlendirme disi kelimeler
+        for i in lessons_dic:
+            # Ders isimleri listeye ekleniyor
+            rownames.append(lessons_dic[i]["code"])
+
+            # O kelimenin Histogram'ı Çıkarılıyor
+            lessons_dic[i]["data"] = {}
+            for a in lessons_dic[i]["content"].split(" "):
+
+                try:
+                    if a.strip() in illegal_words:
+                        break
+                    if lessons_dic[i]["data"][a.strip()]:
+                        lessons_dic[i]["data"][a.strip()] += 1
+                except KeyError:
+                    allwords.add(a.strip())
+                    lessons_dic[i]["data"][a.strip()] = 1
+
+        # Histogramdan elde edilen veriler listelere atiliyor
+        colnames = list(i for i in allwords)
+        for i in lessons_dic:
+            # eger ders icerisinde kelime var ise adedi yok ise degeri 0 veriliyor
+            lesson_temp_list = []
+            for a in colnames:
+                try:
+                    if lessons_dic[i]["data"][a]:
+                        lesson_temp_list.append(lessons_dic[i]["data"][a])
+                except:
+                    lesson_temp_list.append(0)
+            data.append(lesson_temp_list)
+
+        return rownames, colnames, data
+
+    ''' 1.Seviye Fonksiyonlarda Kullanılan Alt Fonksiyonlar '''
 
     def get_page(self, bolumadi):
+        '''Verilen Url'de Bulunan Sayfayı geri döndürür'''
         url = "https://muhendislik.istinye.edu.tr/tr/bolumler/{}/ders-icerikleri".format(
             self.department_names_dic[bolumadi])
         response = myurllib2.urlopen(url)
@@ -31,29 +91,18 @@ class IstinyeDersAnaliz():
         return lesson_url
 
     def get_lesson_content(self, lesson_url):
-        import re
         lesson_dic = {}
         url = lesson_url
         response = myurllib2.urlopen(url)
         soup = BeautifulSoup(response.read(), 'html.parser')
-        print(soup)
         for i in soup.find_all(attrs={"class": "derscontainer"}):
             if len(i("td")[10].text) > 0:  # Bu bölüm için içerik varmı kontrolü
                 raw_lesson_code = i("td")[5].text
                 raw_name = i("td")[6].text
                 raw_content = i("td")[10].text
-                re.sub(r'\r\n', '', raw_content)
-                lesson_dic[raw_lesson_code] = {"name": raw_name, "content": raw_content}
-                print(lesson_dic[raw_lesson_code] )
+                lesson_dic[raw_lesson_code] = {"code": raw_lesson_code, "name": raw_name, "content": raw_content}
         return lesson_dic
 
-    def make_data(self, lessons_dic):
-        rownames, colnames, data = [], [], []
-        for i in lessons_dic:
-            rownames.append(i)
-            line = lessons_dic[i]["content"]
-            colnames.append(line)
-        return rownames, colnames, data
 
 if __name__ == '__main__':
     IstinyeAnaliz = IstinyeDersAnaliz()
